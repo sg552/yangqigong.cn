909039079e06e7607e96700e5f41000af3c392f7
Siwei <sg552sg552@gmail.com>
2014-07-20 21:21:27 +0800

    修正了文章列表的BUG(显示的内容重复两次)

--- app/controllers/refinery/blog/categories_controller.rb
+++ app/controllers/refinery/blog/categories_controller.rb
@@ -6,7 +6,7 @@ module Refinery
       def show
         @category = Refinery::Blog::Category.find(params[:id])
         @posts = @category.posts.live.includes(:comments, :categories).
-          page(params[:page])
+          page(params[:page]).per_page(30)
       end
 
     end
